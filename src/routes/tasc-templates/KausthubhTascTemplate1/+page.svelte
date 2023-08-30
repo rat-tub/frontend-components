001834b902d08cada8d46f6e5e9027a04fa3f738
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-08-31 00:11:32 +0530

    feat: working hamburger menu
    update tasc-template 1 so that its kind of responsive with a hamburger menu

--- src/routes/tasc-templates/KausthubhTascTemplate1/+page.svelte
+++ src/routes/tasc-templates/KausthubhTascTemplate1/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import tasc from "$lib/images/tasc-low-quality.jpg";
+</script>
+
 <svelte:head>
   <title>TASC</title>
   <style>
@@ -13,7 +17,9 @@
 </svelte:head>
 
 <nav class="navbar">
-  <div class="brand-title">TASC-LOGO</div>
+  <img src={tasc} alt="tasc logo" class="brand-title" />
+  <input type="checkbox" class="toggle-menu" />
+  <div class="hamburger" />
   <div class="nav-links">
     <ul>
       <li><a href="/">Home</a></li>
@@ -38,40 +44,50 @@
   .background {
     background-image: url($lib/images/better.jpg);
     background-repeat: repeat;
-    background-color: rgba(0, 0, 0, 0.6); /* Fallback background color */
+    background-color: rgba(0, 0, 0, 0.6);
     background-blend-mode: darken;
     height: 100vh;
   }
 
   .navbar {
+    flex-direction: row;
     display: flex;
     position: fixed;
     justify-content: center;
     align-items: center;
     background-image: url($lib/images/better.jpg);
-    background-color: rgba(0, 0, 0, 0.6); /* Fallback background color */
+    background-color: rgba(0, 0, 0, 0.6);
     background-blend-mode: darken;
     background-repeat: repeat;
     width: 100%;
     color: white;
+    height: 8vh;
   }
 
   .brand-title {
+    cursor: pointer;
+    width: 60px;
+    border-radius: 5px;
     position: absolute;
     left: 0;
     font-size: 1.35rem;
-    margin: 0.5rem;
+    margin: 1rem;
     justify-self: flex-start;
   }
 
   .nav-links {
     height: 100%;
+    display: flex;
+    flex-direction: column;
   }
 
   .nav-links ul {
+    align-items: center;
     display: flex;
     margin: 0;
     padding: 0;
+    height: 100%;
+    gap: 10px;
   }
 
   .nav-links li {
@@ -81,9 +97,102 @@
   .nav-links li a {
     font-size: 20px;
     font-weight: bold;
-    display: block;
     text-decoration: none;
     color: #ffffff;
-    padding: 1rem;
+    padding: 0.5rem 1rem;
+    border-radius: 200px;
+    height: 100%;
+  }
+  .nav-links li a:hover {
+    background-color: white;
+    color: black;
+    transition: 0.3s;
+  }
+  .toggle-menu {
+    opacity: 0;
+    position: relative;
+    top: 100%;
+    width: 30px;
+    display: block; /* Show checkbox for toggling menu */
+  }
+
+  .hamburger {
+    position: relative;
+    display: none;
+    width: 30px;
+    height: 3px;
+    background-color: white;
+    transition: 0.3s;
+  }
+  .hamburger::before,
+  .hamburger::after {
+    position: absolute;
+    content: "";
+    background-color: white;
+    height: 3px;
+  }
+  .hamburger::before {
+    top: -10px;
+    width: 30px;
+  }
+  .hamburger::after {
+    top: 10px;
+    width: 30px;
+  }
+
+  @media (max-width: 640px) {
+    .navbar {
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 1rem;
+      height: auto;
+    }
+
+    .nav-links ul {
+      display: none;
+    }
+
+    .brand-title {
+      position: static;
+      margin: 1rem 0;
+    }
+
+    .toggle-menu:checked ~ .nav-links {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+    }
+
+    .toggle-menu:checked ~ .nav-links ul {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      padding: 2rem;
+    }
+
+    .toggle-menu:checked ~ .nav-links ul li {
+      padding: 0.25rem;
+    }
+
+    .toggle-menu,
+    .hamburger {
+      display: flex;
+      position: relative;
+      align-self: flex-end;
+      top: -50px;
+    }
+    .toggle-menu:checked ~ .hamburger {
+      background-color: transparent;
+    }
+
+    .toggle-menu:checked ~ .hamburger::after {
+      transform: rotate(45deg);
+    }
+
+    .toggle-menu:checked ~ .hamburger::before {
+      transform: rotate(-45deg);
+      top: 10px;
+    }
   }
 </style>
