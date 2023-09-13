a7e07113461fdbaab308dd1023dc8e0a7b0e556a
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-13 21:10:47 +0530

    refactor: create navbar horizontal menu item component

---
+++ src/lib/components/menu/TopMenuItem.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string | null = null;
+  export let target: string = '_self';
+</script>
+
+<li class="flex justify-center items-center">
+  <a {href} {target} class="text-black dark:text-white px-[10px] py-[5px] hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black rounded-lg duration-350 ease-in-out"><slot /></a>
+</li>
--- src/routes/tasc-templates/prathwik1/+page.svelte
+++ src/routes/tasc-templates/prathwik1/+page.svelte
@@ -2,6 +2,7 @@
   import Hamburger from '$lib/components/hamburger/Hamburger.svelte';
   import Menu from '$lib/components/hamburger/Menu.svelte';
   import MenuItem from '$lib/components/hamburger/MenuItem.svelte';
+  import TopMenuItem from '$lib/components/menu/TopMenuItem.svelte';
 
   import logoLight from '$lib/assets/TASCLogoLight.png';
   import logoDark from '$lib/assets/TASCLogoDark.png';
@@ -18,44 +19,39 @@
       margin: 0;
       padding: 0;
 
-      font-family: 'Avenir Next', 'Avenir', sans-serif;
+      /* font-family: 'Avenir Next', 'Avenir', sans-serif; */
     }
   </style>
 </svelte:head>
 
-<nav>
-  <div class="flex justify-between m-5 sm:m-10">
-    <div>
-      {#if $darkTheme === false}
-        <img src={logoLight} alt="TASC logo - light theme" style="height: 2rem;" />
-      {:else}
-        <img src={logoDark} alt="TASC logo - dark theme" style="height: 2rem;" />
-      {/if}
-    </div>
-    <div class="hidden md:flex gap-6">
-      <li class="flex justify-center items-center">
-        <a href={null} class="text-black dark:text-white px-[10px] py-[5px] hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black rounded-lg duration-350 ease-in-out">Home</a>
-      </li>
-      <li class="flex justify-center items-center">
-        <a href={null} class="text-black dark:text-white px-[10px] py-[5px] hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black rounded-lg duration-350 ease-in-out">About</a>
-      </li>
-      <li class="flex justify-center items-center">
-        <a href={null} class="text-black dark:text-white px-[10px] py-[5px] hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black rounded-lg duration-350 ease-in-out">Contact</a>
-      </li>
-    </div>
-    <div class="md:hidden">
-      <Menu {menuToggle}>
-        <MenuItem>Home</MenuItem>
-        <MenuItem>About</MenuItem>
-        <MenuItem>Contact</MenuItem>
-        <MenuItem href="https://github.com/tasc-nmamit" target="_blank">Github</MenuItem>
-      </Menu>
-    </div>
-    <div class="flex">
-      <ThemeToggle2 />
-      <div class="contents md:hidden">
-        <Hamburger bind:checked={menuToggle} />
-      </div>
+<nav class="flex justify-between items-center m-5 sm:m-10">
+  <div>
+    {#if $darkTheme === false}
+      <img src={logoLight} alt="TASC logo - light theme" class="h-6" />
+    {:else}
+      <img src={logoDark} alt="TASC logo - dark theme" class="h-6" />
+    {/if}
+  </div>
+
+  <div class="hidden md:flex gap-6">
+    <TopMenuItem>Home</TopMenuItem>
+    <TopMenuItem>About</TopMenuItem>
+    <TopMenuItem>Contact</TopMenuItem>
+  </div>
+
+  <div class="md:hidden">
+    <Menu {menuToggle}>
+      <MenuItem>Home</MenuItem>
+      <MenuItem>About</MenuItem>
+      <MenuItem>Contact</MenuItem>
+      <MenuItem href="https://github.com/tasc-nmamit" target="_blank">Github</MenuItem>
+    </Menu>
+  </div>
+
+  <div class="flex">
+    <ThemeToggle2 />
+    <div class="contents md:hidden">
+      <Hamburger bind:checked={menuToggle} />
     </div>
   </div>
 </nav>
