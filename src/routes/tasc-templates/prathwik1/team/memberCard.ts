c0223a43ba462ea81b9611cb83bd6e7653fb6512
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-15 08:22:36 +0530

    feat: TS interfaces for event and member items

---
+++ src/routes/tasc-templates/prathwik1/team/memberCard.ts
@@ -0,0 +1,10 @@
+export interface PersonCard {
+  image: string; //link to image
+  imageAlt: string;
+  name: string;
+  role: string;
+  quote: string;
+  github?: string;
+  linkedin?: string;
+  instagram?: string;
+}
