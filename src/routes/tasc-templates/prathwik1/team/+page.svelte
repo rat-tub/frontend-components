6d5bd85a943c4faebc20025ec0f7df66551deb2f
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-15 17:36:48 +0530

    feat: make team team page responsive

--- src/routes/tasc-templates/prathwik1/team/+page.svelte
+++ src/routes/tasc-templates/prathwik1/team/+page.svelte
@@ -1,5 +1,45 @@
 <script lang="ts">
-  import PersonCard from '$lib/components/personcard/PersonCard.svelte';
+  import Card from '$lib/components/profileCard2/Card.svelte';
 </script>
 
-<PersonCard />
+<div class="grid-container pt-5 px-4">
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+</div>
+
+<style>
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+    justify-items: center;
+    justify-content: end;
+    align-content: end;
+    gap: 10px;
+  }
+</style>
