a6f93d18bff953e9632041375159393e046bb5dd
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-17 02:58:54 +0530

    feat: create team route in final tasc-template

---
+++ src/routes/tasc-templates/final/team/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Card from '$lib/components/profileCard/Card.svelte';
+  import { team } from './team';
+</script>
+
+<!-- <div class="grid justify-center md:grid-cols-2 lg:grid-cols-4 gap-y-10 grid-col-1 max-w-8xl p-12 mx-24">
+  {#each personDetails as dummy}
+    <div class="self-center justify-self-center">
+      <CardItem {dummy} />
+    </div>
+  {/each}
+</div> -->
+
+<!-- <div class="grid-container gap-y-5 gap-x-5 p-16"></div> -->
+<div class="grid-container pt-5 px-4">
+  {#each team as profile}
+    <Card {...profile} />
+  {/each}
+</div>
+
+<!-- <div class="self-center justify-self-center"></div> -->
+<!-- grid justify-center gap-y-10 grid-flow-dense auto-row-max md:grid-cols-2 lg:grid-cols-4 grid-col-1 -->
+
+<style>
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+    justify-items: center;
+    justify-content: end;
+    align-content: end;
+    /* Define your column size and minimum width */
+    row-gap: 4rem;
+  }
+</style>
