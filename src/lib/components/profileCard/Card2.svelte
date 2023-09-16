04e0cb2635b1de0f8435b240b24a507120bb98ca
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-17 02:53:23 +0530

    refactor: profileCard component

--- src/lib/components/profileCard/Card2.svelte
+++ src/lib/components/profileCard/Card2.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-  import type PersonCardType from './PersonType';
-  import { browser } from '$app/environment';
-  export let dummy: PersonCardType;
+  export let image: string;
+  export let imageAlt: string = 'profile';
+  export let name: string;
+  export let role: string;
+  export let quote: string = '';
+  export let github: string | null = null;
+  export let linkedin: string | null = null;
+  export let instagram: string | null = null;
+  export let email: string | null = null;
+
   import Icon from 'svelte-icons-pack/Icon.svelte';
   import BsGithub from 'svelte-icons-pack/bs/BsGithub';
   import BsInstagram from 'svelte-icons-pack/bs/BsInstagram';
   import BsLinkedin from 'svelte-icons-pack/bs/BsLinkedin';
+
+  import { browser } from '$app/environment';
 
   if (browser) {
     const cards: NodeListOf<HTMLElement> = document.querySelectorAll('.card');
@@ -35,16 +44,16 @@
 
 <div class="bg-[#f3e7fb] card rounded-lg overflow-hidden shadow-xl dark:custom-shadow-black py-5 border border-[rgb(237,210,255)] w-72 dark:border-slate-700 cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out dark:bg-[#272c3a]">
   <div class="flex items-center justify-center">
-    <img src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?cs=srgb&dl=pexels-sindre-fs-1040881.jpg&fm=jpg" alt={dummy.imageAlt} class="rounded-full w-32 h-32 object-cover border border-[#e5c6fc] dark:border-[#121928]" />
+    <img src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?cs=srgb&dl=pexels-sindre-fs-1040881.jpg&fm=jpg" alt={imageAlt} class="rounded-full w-32 h-32 object-cover border border-[#e5c6fc] dark:border-[#121928]" />
   </div>
   <div class="flex flex-col justify-center items-center">
-    <h1 class="text-2xl font-bold pt-4">{dummy.name}</h1>
-    <h2 class="text-base font-medium dark:text-slate-400 text-zinc-600 p-2">{dummy.role}</h2>
+    <h1 class="text-2xl font-bold pt-4">{name}</h1>
+    <h2 class="text-base font-medium dark:text-slate-400 text-zinc-600 p-2">{role}</h2>
 
     <div class="flex gap-5 py-2">
-      <a href={dummy.github} target="_blank"><Icon src={BsGithub} className="h-5 w-5" title="Custom icon params" /></a>
-      <a href={dummy.linkedin} target="_blank"> <Icon src={BsInstagram} className="h-5 w-5" title="Custom icon params" /></a>
-      <a href={dummy.instagram} target="_blank"> <Icon src={BsLinkedin} className="h-5 w-5" title="Custom icon params" /></a>
+      <a href={github} target="_blank"><Icon src={BsGithub} className="h-5 w-5" title="Custom icon params" /></a>
+      <a href={linkedin} target="_blank"> <Icon src={BsInstagram} className="h-5 w-5" title="Custom icon params" /></a>
+      <a href={instagram} target="_blank"> <Icon src={BsLinkedin} className="h-5 w-5" title="Custom icon params" /></a>
     </div>
   </div>
 </div>
@@ -55,16 +64,16 @@
     <!-- Larger card content -->
     <div class="text-center p-3">
       <img src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?cs=srgb&dl=pexels-sindre-fs-1040881.jpg&fm=jpg" alt="Profile" class="rounded-lg w-72 h-72 object-cover border border-[#e5c6fc] dark:border-[#121928]" />
-      <h1 class="text-3xl font-bold mt-4">{dummy.name}</h1>
-      <h2 class="text-lg font-medium dark:text-slate-400 text-zinc-600 py-3">{dummy.role}</h2>
+      <h1 class="text-3xl font-bold mt-4">{name}</h1>
+      <h2 class="text-lg font-medium dark:text-slate-400 text-zinc-600 py-3">{role}</h2>
       <div class="flex gap-5 py-3 justify-center">
-        <a href={dummy.github} target="_blank"><Icon src={BsGithub} className="h-5 w-5" title="Custom icon params" /></a>
-        <a href={dummy.linkedin} target="_blank"> <Icon src={BsInstagram} className="h-5 w-5" title="Custom icon params" /></a>
-        <a href={dummy.instagram} target="_blank"> <Icon src={BsLinkedin} className="h-5 w-5" title="Custom icon params" /></a>
+        <a href={github} target="_blank"><Icon src={BsGithub} className="h-5 w-5" title="Custom icon params" /></a>
+        <a href={linkedin} target="_blank"> <Icon src={BsInstagram} className="h-5 w-5" title="Custom icon params" /></a>
+        <a href={instagram} target="_blank"> <Icon src={BsLinkedin} className="h-5 w-5" title="Custom icon params" /></a>
       </div>
     </div>
     <div class="w-[100%] border-[0.5px] border-slate-300 mb-5 mt-2" />
-    <p class="p-12 text-center">{dummy.quote}</p>
+    <p class="p-12 text-center">{quote}</p>
     <button class="mt-4 mx-auto block px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700" id="closeLargerCard">Close</button>
   </div>
 </div>
