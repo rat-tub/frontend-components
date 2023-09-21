ec6069a8e956ba53d13bc14aaeb76f09287068fd
prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-21 11:47:59 +0530

    feat: create css/layouts route
    Layouts is where we can create examples of entire page layouts

---
+++ src/routes/css/layouts/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+<div>
+    <ol>
+        <li><a href="{base}/css/layouts/scrollable_div">1 Scrollable Div</a></li>
+    </ol>
+</div>
+
+<style>
+    div {
+        background-color: #ea6363;
+        padding: 10%;
+        border: 10px solid rgb(233, 156, 156);
+        margin: 10%;
+    }
+</style>
