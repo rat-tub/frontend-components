8cba7134248b3e47debbb939ad06e837d07a3e95
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-21 19:52:46 +0530

    feat: create basic scrollable layouts

--- src/routes/css/layouts/+page.svelte
+++ src/routes/css/layouts/+page.svelte
@@ -1,18 +1,19 @@
 <script>
-    import { base } from '$app/paths';
+	import { base } from '$app/paths';
 </script>
 
 <div>
-    <ol>
-        <li><a href="{base}/css/layouts/scrollable_div">1 Scrollable Div</a></li>
-    </ol>
+	<ol>
+		<li><a href="{base}/css/layouts/scrollable">Scrollable Div - CSS</a></li>
+		<li><a href="{base}/css/layouts/scrollable2">Scrollable Div - Tailwind</a></li>
+	</ol>
 </div>
 
 <style>
-    div {
-        background-color: #ea6363;
-        padding: 10%;
-        border: 10px solid rgb(233, 156, 156);
-        margin: 10%;
-    }
+	div {
+		background-color: #ea6363;
+		padding: 10%;
+		border: 10px solid rgb(233, 156, 156);
+		margin: 10%;
+	}
 </style>
---
+++ src/routes/css/layouts/scrollable2/+page.svelte
@@ -0,0 +1,25 @@
+<div class="flex flex-col overflow-hidden h-screen">
+	<div class="flex flex-grow-0 flex-shrink-0 bg-slate-600 text-gray-100">Header (non scrollable)</div>
+	<div class="flex-auto w-full overflow-scroll whitespace-nowrap hideScrollbar">
+		<ul>
+			<li>Menu Item Menu Entry Menu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu Entry ntryMenu EntryMenu EntryMenu EntryMenu EntryMenu EntryMenu Entry</li>
+			{#each { length: 50 } as _, i}
+				<li>Menu Item - {i}</li>
+			{/each}
+		</ul>
+	</div>
+	<div class="flex flex-grow-0 flex-shrink-0 bg-slate-600 text-gray-100">Footer (non scrollable)</div>
+</div>
+
+<style>
+	/* Hide scrollbar for Chrome, Safari and Opera */
+	.hideScrollbar::-webkit-scrollbar {
+		display: none;
+	}
+
+	/* Hide scrollbar for IE, Edge and Firefox */
+	.hideScrollbar {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+</style>
